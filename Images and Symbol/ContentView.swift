94e158ee3a605b9150228292d45b2ea26c119046
MohamedGoush <0x1110111@gmail.com>
2023-10-30 04:02:21 +0530

    Initial Commit

---
+++ Images and Symbol/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Images and Symbol
+//
+//  Created by python on 30/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Images and Symbol/Images_and_SymbolApp.swift
@@ -0,0 +1,17 @@
+//
+//  Images_and_SymbolApp.swift
+//  Images and Symbol
+//
+//  Created by python on 30/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Images_and_SymbolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
