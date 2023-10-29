075ec7ca4706dc4dd532b008d2752b1e5e34bc01
MohamedGoush <0x1110111@gmail.com>
2023-10-30 04:25:40 +0530

    ContentView Changes Commited

--- Images and Symbol/ContentView.swift
+++ Images and Symbol/ContentView.swift
@@ -8,12 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var shouldIBounce = false
     var body: some View {
         VStack {
+            Image(.bellagio)
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(20)
+            
             Image(systemName: "globe")
+                .font(.largeTitle)
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .foregroundColor(.blue)
+                .symbolEffect(.pulse, options: .repeat(3))
+            
+            Image(systemName: "wifi")
+                .font(.largeTitle)
+                .imageScale(.large)
+                .foregroundColor(.blue)
+                .symbolEffect(.variableColor)
+            
+            Image(systemName: "wifi")
+                .font(.largeTitle)
+                .imageScale(.large)
+                .foregroundColor(.blue)
+                .symbolEffect(.variableColor.reversing)
+            
+            Image(systemName: "person.crop.circle.badge.plus")
+                .font(.largeTitle)
+                .imageScale(.large)
+                .foregroundColor(.blue)
+                .symbolRenderingMode(.multicolor)
+                .symbolEffect(.bounce, value: shouldIBounce)
+                .onTapGesture {
+                    shouldIBounce.toggle()
+                }
+            
+            Image(systemName: "cloud.sun.rain.fill")
+                .font(.largeTitle)
+                .imageScale(.large)
+                .foregroundStyle(.gray, .yellow, .mint)
+//                .symbolRenderingMode(.multicolor)
+                .symbolEffect(.bounce, value: shouldIBounce)
+                .onTapGesture {
+                    shouldIBounce.toggle()
+                }
+           
         }
         .padding()
     }
